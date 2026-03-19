c0c0dd31bdeb6ba11c18ee3f8bebd38539d9eb6b
BiiiHaaa <chaoufi.brahim@univ-bechar.dz>
2026-03-19 01:07:42 +0100

    Just 2 simple exo 0001 and 0002

---
+++ 0001/Main.py
@@ -0,0 +1,13 @@
+#Creates a variable called name and store your name in it
+name = "BiiiHaaa"
+#Creates a variable called age and store your age
+age = 22
+#Creates a list called skills with at least 3 skills (strings)
+skills = ["Sleep" ,  "eat" , "Sport" , "code"]
+#Print this sentence using variables:
+#Hello, my name is ___ and I am ___ years old.
+print("Hello, my name is {} and i am {} years Old ".format(name , age) )
+#Print the first skill from the list
+print(skills[0]) 
+#Print the number of skills you have (using something you learned)
+print(len(skills))
---
+++ 0002/Main.py
@@ -0,0 +1,11 @@
+#Create a list of numbers:
+numbers = [10, 20, 30, 40, 50]
+#Print the first number
+print(numbers[0])
+#Print the last number
+print(numbers[-1])
+#Print the sum of the first and last number
+Sum = (numbers[0] + numbers[-1])
+print(Sum)
+#Print the type of the list
+print(type(numbers))
