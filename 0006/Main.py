7682a968a2cbc1f732b28170ce0e2d6d681e0175
BiiiHaaa <chaoufi.brahim@univ-bechar.dz>
2026-03-21 01:00:30 +0100

    add 0003 and 0004 and 0005 Exo

---
+++ 0006/Main.py
@@ -0,0 +1,12 @@
+#Create:
+product = "Laptop"
+price = 1500
+budget = 1200
+#Calculate the difference between price and budget
+difference = price - budget
+#Print:
+#You need ___ more to buy Laptop
+print(f"You need {difference} more to buy Laptop")
+#Also print:
+#Is the budget enough? ___
+print(f"Is the budget enough ? {price<=budget}")
